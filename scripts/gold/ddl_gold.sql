7b32ac2dc66b619e90e8040865937762e11fdeaf
sanchezdiego43 <sanchez.diego43@gmail.com>
2025-08-25 10:36:30 -0500

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -18,67 +18,81 @@ Usage:
 -- CUSTOMERS BUSINESS OBJECT CREATION
 -- The GOLD LAYER starts from the base of the SILVER LAYER
 -- ALWAYS START QUERYING FROM THE MASTER TABLE OF THE BUSINESS OBJECT
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+    DROP VIEW gold.dim_customers;
+GO
+
 CREATE VIEW gold.dim_customers AS
 SELECT 
-ROW_NUMBER() OVER(ORDER BY ci.cst_id) AS customer_key,
-ci.cst_id AS customer_id,
-ci.cst_key AS customer_number,
-ci.cst_fistname AS first_name,
-ci.cst_lastname AS last_name,
-la.CNTRY AS country,
-CASE
-	WHEN ci.cst_gnder != 'n/a' THEN ci.cst_gnder
-	ELSE COALESCE(ca.GEN,'n/a')
-END AS genre,
-ci.cst_marital_status AS marital_status,
-ca.BDATE AS birthdate,
-ci.cst_create_date AS creation_date
+    ROW_NUMBER() OVER(ORDER BY ci.cst_id) AS customer_key,
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_fistname AS first_name,
+    ci.cst_lastname AS last_name,
+    la.CNTRY AS country,
+    CASE
+	    WHEN ci.cst_gnder != 'n/a' THEN ci.cst_gnder
+	    ELSE COALESCE(ca.GEN,'n/a')
+    END AS genre,
+    ci.cst_marital_status AS marital_status,
+    ca.BDATE AS birthdate,
+    ci.cst_create_date AS creation_date
 FROM silver.crm_cust_info AS ci  --ci for Cust Info
 LEFT JOIN silver.erp_CUST_AZ12 AS ca --ca for CUST AS12
 ON ci.cst_key = ca.CID
 LEFT JOIN silver.erp_LOC_A101 AS la --la for LOC A101}
 ON ci.cst_key = la.CID;
+GO
 
 -- PRODUCT BUSINESS OBJECT CREATION
 -- The GOLD LAYER starts from the base of the SILVER LAYER
 -- ALWAYS START QUERYING FROM THE MASTER TABLE OF THE BUSINESS OBJECT
-DROP VIEW IF EXISTS gold.dim_product;
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
 
-CREATE VIEW gold.dim_product AS
+
+CREATE VIEW gold.dim_products AS
 SELECT 
-ROW_NUMBER() OVER(ORDER BY ppi.prd_id) AS product_key, 
-ppi.prd_id AS product_id,
-ppi.prd_key AS product_number,
-ppi.prd_nm AS product_name,
-ppi.category_id AS category_id,
-pcg.CAT AS category,
-pcg.SUBCAT AS subcategory,
-pcg.MAINTENANCE AS maintenance,
-ppi.prd_cost AS product_cost,
-ppi.prd_line AS product_line,
-ppi.prd_start_dt AS start_date
+    ROW_NUMBER() OVER(ORDER BY ppi.prd_id) AS product_key, 
+    ppi.prd_id AS product_id,
+    ppi.prd_key AS product_number,
+    ppi.prd_nm AS product_name,
+    ppi.category_id AS category_id,
+    pcg.CAT AS category,
+    pcg.SUBCAT AS subcategory,
+    pcg.MAINTENANCE AS maintenance,
+    ppi.prd_cost AS product_cost,
+    ppi.prd_line AS product_line,
+    ppi.prd_start_dt AS start_date
 FROM silver.crm_prd_info AS ppi
 LEFT JOIN silver.erp_PX_CAT_G1V2 AS pcg
 ON ppi.category_id = pcg.ID
 WHERE ppi.prd_end_dt IS NULL; --Filter historical data
+GO
 
 
 -- SALES BUSINESS OBJECT CREATION
 -- The GOLD LAYER starts from the base of the SILVER LAYER
 -- ALWAYS START QUERYING FROM THE MASTER TABLE OF THE BUSINESS OBJECT
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+
 CREATE VIEW gold.fact_sales AS
-SELECT 
-sd.sls_ord_num AS order_number,
-dp.product_key,
-dc.customer_key,
-sd.sls_order_dt AS order_date,
-sd.sls_ship_dt AS ship_date,
-sd.sls_due_dt AS due_date,
-sd.sls_sales AS sales_amount,
-sd.sls_quantity AS quantity,
-sd.sls_price AS price
-FROM silver.crm_sales_details AS sd
-LEFT JOIN gold.dim_customers AS dc
-ON sd.sls_cust_id = dc.customer_id
-LEFT JOIN gold.dim_product AS dp
-ON sd.sls_prd_key = dp.product_key;
+SELECT
+    sd.sls_ord_num  AS order_number,
+    pr.product_key  AS product_key,
+    cu.customer_key AS customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+    ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+    ON sd.sls_cust_id = cu.customer_id;
+GO
